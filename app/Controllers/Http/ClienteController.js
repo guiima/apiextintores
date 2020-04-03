a2a4fddb9d0d15e1794ce0e3e42902cc0e8a1dc4
Rafael <rafael_guiimaraes@hotmail.com>
2020-04-03 17:45:42 -0300

    upadate de telefones e emails de usuarios finalidado

--- app/Controllers/Http/ClienteController.js
+++ app/Controllers/Http/ClienteController.js
@@ -2,6 +2,8 @@
 
 const Database = use("Database");
 const Cliente = use("App/Models/Cliente");
+const Telefone = use("App/Models/Telefone");
+const Email = use("App/Models/Email");
 
 class ClienteController {
   async index() {
@@ -58,41 +60,85 @@ class ClienteController {
       .where("id", params.id)
       .fetch();
 
+    console.log("entrei no show de clientes");
+
     return data;
   }
 
   async update({ params, request }) {
-    const cliente = await Cliente.query()
-      .with("usuario")
-      .with("telefone")
-      .with("email")
-      .where("id", params.id)
-      .first()
-      .fetch();
+    const cliente = await Cliente.findOrFail(params.id);
 
-    const data = request.only([
-      "nome_fantasia",
-      "razao_social",
-      "tipo_pessoa",
-      "metragem",
-      "cpf",
-      "cnpj",
-      "endereco",
-      "cep",
-      "mei",
-      "data_nascimento",
-      "responsavel",
-      "observacao",
-      "inscricao_estadual",
-      "privado",
-      "status",
-      "usuario_id"
-    ]);
+    const { telefone, email, ...data } = request.body;
 
-    cliente.merge(data);
+    // console.log("foneee", telefone);
+    // console.log("email", email);
+    // console.log("dataaa", data);
+    // console.log("cliente", cliente);
+
+    const trx = await Database.beginTransaction();
+
+    cliente.merge(data, trx);
     await cliente.save();
 
-    return cliente;
+    await Database.table("telefones", trx)
+      .where("cliente_id", cliente.id)
+      .delete();
+
+    await Database.table("emails", trx)
+      .where("cliente_id", cliente.id)
+      .delete();
+
+    const newFones = telefone.map(fone => ({
+      numero: fone.numero,
+      whats_app: fone.whats_app,
+      cliente_id: fone.cliente_id
+    }));
+
+    const newEmails = email.map(mail => ({
+      email: mail.email,
+      cliente_id: mail.cliente_id
+    }));
+
+    await cliente.telefone().createMany(newFones, trx);
+    await cliente.email().createMany(newEmails, trx);
+
+    await trx.commit();
+
+    return request.body;
+
+    // const cliente = await Cliente.query()
+    //   .with("usuario")
+    //   .with("telefone")
+    //   .with("email")
+    //   .where("id", params.id)
+    //   .fetch();
+
+    // const { telefone, email, ...data } = request.only([
+    //   "nome_fantasia",
+    //   "razao_social",
+    //   "tipo_pessoa",
+    //   "metragem",
+    //   "cpf",
+    //   "cnpj",
+    //   "endereco",
+    //   "cep",
+    //   "mei",
+    //   "data_nascimento",
+    //   "responsavel",
+    //   "observacao",
+    //   "inscricao_estadual",
+    //   "privado",
+    //   "status",
+    //   "usuario_id"
+    // ]);
+
+    // cliente.merge(data);
+    // await cliente.save();
+
+    // console.log("foneee", telefone);
+    // console.log("foneee", email);
+
+    // return cliente;
   }
 
   async destroy({ params }) {
